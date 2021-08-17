c06f3a634e053db3f9687f9400c0f0d23d4a2a1a
masarabbit <masahitoart@gmail.com>
2021-08-18 00:08:46 +0100

    map update and ladder logic

--- scripts/walkSeven.js
+++ scripts/walkSeven.js
@@ -6,6 +6,8 @@
 
 //! design other avatars and map
 
+//! home needs to have separate tile to prevent bear penetrating wall
+
 
 function init() {
   
@@ -64,7 +66,6 @@ function init() {
       speed: 2000
     }     
   }
-
   const randomColor = () =>{
     const r = ()=> Math.ceil(Math.random() * 255)
     return `rgb(${r()},${r()},${r()})`
@@ -77,7 +78,6 @@ function init() {
     return `rgb(${r()},${g()},${b()})`
   }
 
-
   const tree = () =>{
     return 'D 5 0h6v1h2v1h1v1h1v1h1v7hNv1hNv1hNv1hTv2hNv-3hNvNhTv1hNv3hNvThTvNhNvNhNvNhNv-7h1vNh1vNh1vNh2vN"/ F 7 12h2v1h1v3h-4v-3h1vN"/'
   }
@@ -87,19 +87,23 @@ function init() {
   }
 
   const buildingCorner = subColor =>{
-    return `D 5 0h11v1hN1vN"/ D 3 1h2v1hTvN"/ <path fill="${subColor ? subColor : 'white'}" d="M 5 1h11v15hN5vN1h1vTh1vNh2vN"/ D 2 2h1v1hNvN"/ D 1 3h1v2hNvT"/ D 0 5h1v11hNvN1"/`
+    return `D 5 0h11v1hN1vN"/ D 3 1h2v1hTvN"/ <path fill="${subColor || 'white'}" d="M 5 1h11v15hN5vN1h1vTh1vNh2vN"/ D 2 2h1v1hNvN"/ D 1 3h1v2hNvT"/ D 0 5h1v11hNvN1"/`
   }
 
   const roofCorner = subColor =>{
-    return `D 0 0h1v11h1v2hNv3hNvN6"/ <path fill="${subColor ? subColor : 'white'}" d="M 1 0h15v15hN1vNhTvNhNvThNvN1"/ F 1 13h1v1h1v1h2v1h-4v-3"/ D 2 13h1v1hNvN"/ D 3 14h2v1hTvN"/ D 5 15h11v1hN1vN"/`
+    return `D 0 0h1v11h1v2hNv3hNvN6"/ <path fill="${subColor || 'white'}" d="M 1 0h15v15hN1vNhTvNhNvThNvN1"/ F 1 13h1v1h1v1h2v1h-4v-3"/ D 2 13h1v1hNvN"/ D 3 14h2v1hTvN"/ D 5 15h11v1hN1vN"/`
+  }
+
+  const checkered = () =>{
+    return 'D 0 0h2v2hTvT"/ D 4 0h2v2hTvT"/ D 8 0h2v2hTvT"/ D 12 0h2v2hTvT"/ D 2 2h2v2hTvT"/ D 6 2h2v2hTvT"/ D 10 2h2v2hTvT"/ D 14 2h2v2hTvT"/ D 0 4h2v2hTvT"/ D 4 4h2v2hTvT"/ D 8 4h2v2hTvT"/ D 12 4h2v2hTvT"/ D 2 6h2v2hTvT"/ D 6 6h2v2hTvT"/ D 10 6h2v2hTvT"/ D 14 6h2v2hTvT"/ D 0 8h2v2hTvT"/ D 4 8h2v2hTvT"/ D 8 8h2v2hTvT"/ D 12 8h2v2hTvT"/ D 2 10h2v2hTvT"/ D 6 10h2v2hTvT"/ D 10 10h2v2hTvT"/ D 14 10h2v2hTvT"/ D 0 12h2v2hTvT"/ D 4 12h2v2hTvT"/ D 8 12h2v2hTvT"/ D 12 12h2v2hTvT"/ D 2 14h2v2hTvT"/ D 6 14h2v2hTvT"/ D 10 14h2v2hTvT"/ D 14 14h2v2hTvT"/'
   }
 
   const plain = subColor =>{
-    return `<path fill="${subColor ? subColor : 'white'}" d="M 0 0h16v16hN6vN6"/`
+    return `<path fill="${subColor || 'white'}" d="M 0 0h16v16hN6vN6"/`
   }
 
   const plainEdge = subColor =>{
-    return `D 0 0h16v1hN6vN"/ <path fill="${subColor ? subColor : 'white'}" d="M 0 1h16v15hN6vN5"/`
+    return `D 0 0h16v1hN6vN"/ <path fill="${subColor || 'white'}" d="M 0 1h16v15hN6vN5"/`
   }
 
   const door = () =>{
@@ -115,11 +119,14 @@ function init() {
   }
 
   const sideSquareWindow = () =>{
+    // return `D 0 0h1v16hNvN6"/ F 1 0h15v16hN5vN6"/ D 7 2h6v1h1v10hNv1h-6vNhNvN0h1vN"/`
+    // return `D 0 0h1v16hNvN6"/ F 1 0h15v16hN5vN6"/ D 9 3h5v1h1v10hNv1h-5vNhNvN0h1vN"/`
+    // return `D 0 0h1v16hNvN6"/ F 1 0h15v16hN5vN6"/ D 10 4h4v1h1v9hNv1h-4vNhNv-9h1vN"/`
     return 'D 0 0h1v16hNvN6"/ F 1 0h15v16h-3vNh1v-9hNvNh-4v1hNv9h1v1h-8vN6"/ D 9 5h4v1h1v9hNv1h-4vNhNv-9h1vN"/'
   }
 
   const noSideWindow = () =>{
-    return 'F 0 0h16v16h-3vNh1v-9hNvNh-4v1hNv9h1v1h-9vN6"/ D 9 5h4v1h1v9hNv1h-4vNhNv-9h1vN"/'
+    return 'F 0 0h16v16h-3vNh1v-9hNvNh-4v1hNv9h1v1h-9vN6"/ D 9 5h4v1h1v9hNv1h-4vNhNv-9h1vN"/`'
   }
 
   const sideSquareWindowAlt = () =>{
@@ -127,38 +134,44 @@ function init() {
   }
 
   const roofCurve = subColor =>{
-    return `<path fill="${subColor ? subColor : 'white'}" d="M 0 0h16v15hTvNhNvNhNvNhTvNh-4v1hTv1hNv1hNv1hTvN5"/ D 6 11h4v1h-4vN"/ D 4 12h2v1hTvN"/ F 6 12h4v1h2v1h1v1h1v1hN2vNh1vNh1vNh2vN"/ D 10 12h2v1hTvN"/ D 3 13h1v1hNvN"/ D 12 13h1v1hNvN"/ D 2 14h1v1hNvN"/ D 13 14h1v1hNvN"/ D 0 15h2v1hTvN"/ D 14 15h2v1hTvN"/`
+    return `<path fill="${subColor || 'white'}" d="M 0 0h16v15hTvNhNvNhNvNhTvNh-4v1hTv1hNv1hNv1hTvN5"/ D 6 11h4v1h-4vN"/ D 4 12h2v1hTvN"/ F 6 12h4v1h2v1h1v1h1v1hN2vNh1vNh1vNh2vN"/ D 10 12h2v1hTvN"/ D 3 13h1v1hNvN"/ D 12 13h1v1hNvN"/ D 2 14h1v1hNvN"/ D 13 14h1v1hNvN"/ D 0 15h2v1hTvN"/ D 14 15h2v1hTvN"/`
   }
 
   const roofTopBottomCorner = subColor =>{
-    return `D 0 0h1v11hNvN1"/ F 1 0h15v15hN1vNhTvNhNvThNvN1"/ <path fill="${subColor ? subColor : 'white'}" d="M 0 11h1v2h1v1h1v1h2v1h-5v-5"/ D 1 11h1v2hNvT"/ D 2 13h1v1hNvN"/ D 3 14h2v1hTvN"/ D 5 15h11v1hN1vN"/`
+    return `D 0 0h1v11hNvN1"/ F 1 0h15v15hN1vNhTvNhNvThNvN1"/ <path fill="${subColor || 'white'}" d="M 0 11h1v2h1v1h1v1h2v1h-5v-5"/ D 1 11h1v2hNvT"/ D 2 13h1v1hNvN"/ D 3 14h2v1hTvN"/ D 5 15h11v1hN1vN"/`
   }
-  const river = () =>{
-    const main = '#adfffe'
-    const sub = '#50fbf9'
 
-    return `<path fill="${main}" d="M 0 0h2v7h2v-7h12v16hTv-7hTv7hN2vN6"/ <path fill="#50fbf9" d="M 2 0h2v7hTv-7"/ <path fill="${sub}" d="M 7 4h2v8hTv-8"/ <path fill="${sub}" d="M 12 9h2v7hTv-7"/`
+  const river = () =>{
+    const main = '#58d3d8'
+    const sub = '#a2fcf0'
+
+    return `<path fill="${main}" d="M 0 0h2v7h2v-7h12v16hTv-7hTv7hN2vN6"/ <path fill="${sub}" d="M 2 0h2v7hTv-7"/ <path fill="${sub}" d="M 7 4h2v8hTv-8"/ <path fill="${sub}" d="M 12 9h2v7hTv-7"/`
   }
 
   const riverAnim = () =>{
-    const main = '#adfffe'
-    const sub = '#50fbf9'
+    const main = '#58d3d8'
+    const sub = '#a2fcf0'
 
-    return `<path fill="${main}" d="M 0 0h7v4h2v-4h7v16h-7v-5hTv5h-7vN6"/ <path fill="#50fbf9" d="M 7 0h2v4hTv-4"/ <path fill="${sub}" d="M 12 3h2v9hTv-9"/ <path fill="${sub}" d="M 2 4h2v9hTv-9"/ <path fill="${sub}" d="M 7 11h2v5hTv-5"/`
+    return `<path fill="${main}" d="M 0 0h7v4h2v-4h7v16h-7v-5hTv5h-7vN6"/ <path fill="${sub}" d="M 7 0h2v4hTv-4"/ <path fill="${sub}" d="M 12 3h2v9hTv-9"/ <path fill="${sub}" d="M 2 4h2v9hTv-9"/ <path fill="${sub}" d="M 7 11h2v5hTv-5"/`
   }
 
   const riverCurve = () =>{
-    const main = '#adfffe'
-    const sub = '#50fbf9'
+    const main = '#58d3d8'
+    const sub = '#a2fcf0'
     
-    return `<path fill="${main}" d="M 8 0h8v6hNv1hTv1hTv2hNv1hNv3h2v-3h1vNh1vNh2vNh1v8hN2v-6h1vNh1vTh1vNh2vNh1vThNv1hTv1hNv1hNv1hNv2hNv1hNv6hTv-8h1vTh1vTh1vNh1vNh2vNh2vN"/ <path fill="${sub}" d="M 9 3h1v2hNv1hTv1hNv2hNv1hNv6hTv-6h1vNh1vTh1vNh1vNh1vNh2vN"/ <path fill="${sub}" d="M 15 6h1v2hNv1hTv1hNv1hNv3hTv-3h1vNh1vTh2vNh2vN"/`
+    // return `<path fill="${main}" d="M 8 0h8v6hNv1hTv1hTv2hNv1hNv3h2v-3h1vNh1vNh2vNh1v8hN2v-6h1vNh1vTh1vNh2vNh1vThNv1hTv1hNv1hNv1hNv2hNv1hNv6hTv-8h1vTh1vTh1vNh1vNh2vNh2vN"/ <path fill="${sub}" d="M 9 3h1v2hNv1hTv1hNv2hNv1hNv6hTv-6h1vNh1vTh1vNh1vNh1vNh2vN"/ <path fill="${sub}" d="M 15 6h1v2hNv1hTv1hNv1hNv3hTv-3h1vNh1vTh2vNh2vN"/`
+    // return `<path fill="${main}" d="M 8 0h8v12hTv1hTv3h-3v-4h1vTh1vNh2vThTv1hNv1hNv1hNv2hNv4h-7v-8h1vTh1vTh1vNh1vNh2vNh2vN"/ <path fill="${sub}" d="M 10 2h5v2h-5v1hTv1hNv1hTvTh2vNh1vNh2vN"/ <path fill="${sub}" d="M 11 7h2v2hTv1hNv2hNv4hTv-4h1vTh1vNh1vNh1vN"/ <path fill="${sub}" d="M 14 12h2v2hTv2hTv-3h2vN"/ <path fill="${main}" d="M 14 14h2v2hTvT"/`
+
+    return `<path fill="${main}" d="M 8 0h8v16h-7v-4h1vTh1vNh2vThTv1hNv1hNv1hNv2hNv4h-7v-8h1vTh1vTh1vNh1vNh2vNh2vN"/ <path fill="${sub}" d="M 10 2h5v2h-5v1hTv1hNv1hTvTh2vNh1vNh2vN"/ <path fill="${sub}" d="M 11 7h2v2hTv1hNv2hNv4hTv-4h1vTh1vNh1vNh1vN"/`
   }
 
   const riverCurveAnim = () =>{
-    const main = '#adfffe'
-    const sub = '#50fbf9'
+    const main = '#58d3d8'
+    const sub = '#a2fcf0'
 
-    return `<path fill="${main}" d="M 8 0h4v1hNv1hTv1hNv1hNv1hNv1hNv1hNv2h2vTh1vNh2vNh1vNh1vNh1vNh2vNh1vNh1v16h-6v-4h1vNh1vNh1vNh1vThNv1hNv1hNv1hNv1hNv1hNv4h-8v-8h1vTh1vTh1vNh1vNh2vNh2vN"/ <path fill="${sub}" d="M 12 0h3v1hNv1hTv1hNv1hNv1hNv1hTv1hNv2hTvTh1vNh1vNh1vNh1vNh1vNh2vNh1vN"/ <path fill="${sub}" d="M 13 7h1v2hNv1hNv1hNv1hNv4hTv-4h1vNh1vNh1vNh1vNh1vN"/`
+    // return `<path fill="${main}" d="M 8 0h4v1hNv1hTv1hNv1hNv1hNv1hNv1hNv2h2vTh1vNh2vNh1vNh1vNh1vNh2vNh1vNh1v16h-6v-4h1vNh1vNh1vNh1vThNv1hNv1hNv1hNv1hNv1hNv4h-8v-8h1vTh1vTh1vNh1vNh2vNh2vN"/ <path fill="${sub}" d="M 12 0h3v1hNv1hTv1hNv1hNv1hNv1hTv1hNv2hTvTh1vNh1vNh1vNh1vNh1vNh2vNh1vN"/ <path fill="${sub}" d="M 13 7h1v2hNv1hNv1hNv1hNv4hTv-4h1vNh1vNh1vNh1vNh1vN"/`
+    return `<path fill="${main}" d="M 8 0h8v2h-3v1h-4v2h4vNh3v8hTv1hTv3h-8v-5h1vThTv2hNv5hTv-8h1vTh1vTh1vNh1vNh2vNh2vN"/ <path fill="${sub}" d="M 13 2h3v2h-3v1h-4vTh4vN"/ <path fill="${sub}" d="M 10 7h3v2h-3v1hNv1hTvTh1vNh2vN"/ <path fill="${sub}" d="M 3 9h2v2hNv5hTv-5h1vT"/ <path fill="${sub}" d="M 14 12h2v2hTv2hTv-3h2vN"/ <path fill="${main}" d="M 14 14h2v2hTvT"/`
+    
   }
 
   const ladder = subColor =>{
@@ -166,13 +179,14 @@ function init() {
     return `<path fill="${subColor || 'white'}" d="M 0 0h16v16hN6vN6"/ D 3 1h1v1hNvN"/ D 12 1h1v1hNvN"/ D 4 2h8v1h-8vN"/ D 3 5h1v1hNvN"/ D 12 5h1v1hNvN"/ D 4 6h8v1h-8vN"/ D 3 9h1v1hNvN"/ D 12 9h1v1hNvN"/ D 4 10h8v1h-8vN"/ D 3 13h1v1hNvN"/ D 12 13h1v1hNvN"/ D 4 14h8v1h-8vN"/`
   }
 
-
-  const sub = '#e2cc9c'
-  const main = '#7d551c'
+  // const sub = '#e2cc9c'
+  const sub = '#f9ede5'
+  // const main = '#7d551c'
+  const main = '#74645a'
 
   const svgData = {
-    't': { svg: tree, color: randomGreen },
-    'w': { svg: tree, color: randomGreen },
+    't': { svg: tree, color: '#0d8799' },
+    'w': { svg: tree, color: '#0d8799' },
     'o': { svg: flowers, color: randomColor },
     'd': { svg: buildingCorner, color: main, subColor: sub },
     's': { svg: buildingCorner, color: main, subColor: sub, rotate: 90 },
@@ -180,6 +194,8 @@ function init() {
     'br': { svg: buildingCorner, color: main, rotate: 90 },
     'bb': { svg: buildingCorner, color: main, rotate: 180 },
     'bl': { svg: buildingCorner, color: main, rotate: 270 },
+    'bx': { svg: roofTopBottomCorner, color: main, subColor: '#0d8799' },
+    'by': { svg: roofTopBottomCorner, color: main, subColor: '#0d8799', flip: 'h' },
     'rbr': { svg: roofTopBottomCorner, color: main, subColor: sub },
     'rbl': { svg: roofTopBottomCorner, color: main, subColor: sub, flip: 'h' },
     'rtr': { svg: roofTopBottomCorner, rotate: 180, color: main, subColor: sub },
@@ -206,14 +222,16 @@ function init() {
     'pr': { svg: plainEdge, color: main, subColor: sub, rotate: 90 },
     'pb': { svg: plainEdge, color: main, subColor: sub, rotate: 180 },
     'pu': { svg: plainEdge, color: main, subColor: sub, rotate: 270 },
-    'b': { svg: plain, subColor: '#afff7a' },
+    'b': { svg: plain, subColor: '#a2fcf0' },
+    'bd': {svg: plain, subColor: '#0d8799'},
     'r': { svg: river, animation: riverAnim },
     'rh': { svg: river, rotate: 90, animation: riverAnim },
     'ra': { svg: riverCurve, animation: riverCurveAnim },
     'rb': { svg: riverCurve, rotate: 90, animation: riverCurveAnim },
     'rd': { svg: riverCurve, rotate: 180, animation: riverCurveAnim },
     're': { svg: riverCurve, rotate: 270, animation: riverCurveAnim },
-    'la': {svg: ladder, color: main, subColor: sub}
+    'la': {svg: ladder, color: main, subColor: sub},
+    'c': {svg: checkered, color: '#a2e8fc'}
   }
 
   const eventPoints = {
@@ -297,14 +315,14 @@ function init() {
       name: 'home',
       cell: 79,
       direction: 'up',
-      noWall: ['bt','at','rtr','ar','bb','ab','bl','p','la']
+      noWall: ['bt','at','rtl','rtr','ar','bx','ab','by','p','la','bd']
     },
     portal7: {
       map: 5,
       name: 'home',
       cell: 62,
       direction: 'up',
-      noWall: ['bt','at','rtr','ar','bb','ab','bl','p','la']
+      noWall: ['bt','at','rtl','rtr','ar','bx','ab','by','p','la','bd']
     },
     portal8: {
       map: 1,
@@ -334,7 +352,7 @@ function init() {
       characters: [
         '155_bunny_0_hello',
         '156_bunny_0_apple',
-        '311_bunny_0_tomato'
+        '309_bunny_0_tomato'
       ],
       events: [
         '5_transport-portal3',
@@ -344,7 +362,7 @@ function init() {
         '253_transport-portal7',
         '288_transport-portal6'
       ],
-      map: 'v5,b2,v24,w4,b2,w22,v2,w1,b14,d1,pt2,s1,b8,w1,v2,w1,b12,t1,b1,g1,pb2,y1,b6,t1,b1,w1,v2,w1,b2,t1,b10,d1,al1,p1,nwr1,swr1,pt1,s1,b6,w1,v2,w1,b10,d1,pt2,pu1,rbr1,do1,ab1,rbl1,rp1,pr1,b6,w1,v2,w1,b6,t1,b3,g1,rc1,pb1,g1,pb5,y1,b6,w1,v2,w1,b10,swl1,p1,nwr1,al1,p5,ar1,b6,w1,v2,w1,b10,bl1,do1,ab1,al1,wi1,p1,sw1,p1,wi1,ar1,b2,d1,pt1,s1,b1,w1,v2,w1,b13,bl1,ab2,do1,ab2,bb1,b2,g1,rc1,y1,b1,w1,v2,w1,b1,t1,b3,t1,b16,swl1,p1,swr1,b1,w1,v2,w1,b10,t1,b8,t1,b2,bl1,do1,bb1,b1,w1,v2,w1,b2,t1,b6,ra1,rh5,rb1,b10,w1,v2,w1,b5,ra1,rh3,rd1,b5,r1,b12,v1,w1,b5,r1,b7,w1,b1,r1,b7,t1,b4,v1,w1,b1,t1,b3,r1,b1,t1,b7,r1,b4,t1,b5,w1,v2,w1,b5,r1,b5,w1,b3,re1,rh2,rb1,b7,w1,v2,w1,b5,r1,b12,r1,b7,w1,v2,w6,r1,w12,r1,w8,v8,r1,v12,r1,v9',
+      map: 'v5,b2,v24,w4,b2,w22,v2,w1,b14,d1,pt2,s1,b8,w1,v2,w1,b12,t1,b1,g1,pb2,y1,b6,t1,b1,w1,v2,w1,b2,t1,b10,d1,al1,p1,nwr1,swr1,pt1,s1,b6,w1,v2,w1,b10,d1,pt2,pu1,rbr1,do1,ab1,rbl1,rp1,pr1,b6,w1,v2,w1,b6,t1,b3,g1,rc1,pb1,g1,pb5,y1,b6,w1,v2,w1,b10,swl1,p1,nwr1,al1,p5,ar1,b6,w1,v2,w1,b10,bl1,do1,ab1,al1,wi1,p1,sw1,p1,wi1,ar1,b2,d1,pt1,s1,b1,w1,v2,w1,b13,bl1,ab2,do1,ab2,bb1,b2,g1,rc1,y1,b1,w1,v2,w1,b1,t1,b3,t1,b3,t1,b12,swl1,p1,swr1,b1,w1,v2,w1,b19,t1,b2,bl1,do1,bb1,b1,w1,v2,w1,b2,t1,b6,ra1,rh5,rb1,b10,w1,v2,w1,b5,ra1,rh3,rd1,b5,r1,b12,v1,w1,b5,r1,b7,w1,b1,r1,b7,t1,b4,v1,w1,b1,t1,b3,r1,b1,t1,b7,r1,b4,t1,b5,w1,v2,w1,b5,r1,b5,w1,b3,re1,rh2,rb1,b7,w1,v2,w1,b5,r1,b12,r1,b7,w1,v2,w6,r1,w12,r1,w8,v8,r1,v12,r1,v9',
     },
     {
       name: 'two',
@@ -408,7 +426,7 @@ function init() {
         '74_transport-portal8',
         '91_transport-portal9',
       ],
-      map: 'v4,rp1,la1,rp5,v5,rp1,la1,rp5,v2,rp4,la1,rp5,v2,rp3,rtl1,at5,rtr1,v2,rtl1,at2,p6,ar1,v2,bl1,p1,ab1,p6,ar1,v3,p1,v1,bl1,ab2,p1,ab2,bb1,v8,p1,v16'
+      map: 'bd4,rp1,la1,rp5,bd5,rp1,la1,rp5,bd2,rp4,la1,rp5,bd2,rp3,rtl1,at5,rtr1,bd2,rtl1,at2,p6,ar1,bd2,bx1,p1,ab1,p6,ar1,bd5,bx1,ab2,p1,ab2,by1,bd25'
     }
   ]
 
@@ -681,7 +699,8 @@ function init() {
       spawn.innerHTML = sprite()
       // spawn.innerHTML = spriteContainer.innerHTML //! this needs to be changed to whatever defined in avatar object
       spawn.classList.add('spawn')
-      spawn.style.fill = randomColor()
+      // spawn.style.fill = randomColor()
+      spawn.style.fill = '#74645a'
       spawnContainer.appendChild(spawn)    
       mapImage.appendChild(spawnContainer)    
       spawnData[i].interval = setInterval(()=>spawnMotion(spawnContainer,i),avatars[c.split('_')[1]].speed)
@@ -871,7 +890,10 @@ function init() {
       default: console.log('invalid command')
         return
     }
-    turnSprite(direction,actor,sprite,true)
+    console.log(mapImageTiles[bear.pos].classList)
+    mapImageTiles[bear.pos].classList.contains('la') || (mapImageTiles[bear.pos - iWidth].classList.contains('la') && direction === 'down')
+      ? turnSprite('up',actor,sprite,true)
+      : turnSprite(direction,actor,sprite,true)
     if (!spawn) locationTiles[actor.pos].classList.add('mark')
 
     if (!spawn && mapImageTiles[bear.pos].dataset.event) {
