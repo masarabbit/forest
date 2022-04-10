f0d027decaa86feacc37c97d32b9c28c0b621c11
masarabbit <masahitoart@gmail.com>
2022-04-11 00:20:50 +0100

    add basic grid for map generator

--- scripts/mapGeneration.js
+++ scripts/mapGeneration.js
@@ -1,6 +1,27 @@
+// import { setTargetSize } from './utils/utils.js'
+
 function init() {
 
-  console.log('test')
+  const mapData = {
+    width: 30,
+    height: 20,
+    cellD: 20,
+    grid: document.querySelector('.grid')
+  }
+
+  const setTargetSize = (target, w, h) => {
+    target.setAttribute('style', `width:${w}px; height:${h}px; --width:${w}px; --height: ${h}px;`)  
+  }
+  
+  const createGrid = () =>{
+    const { width, height, cellD } = mapData
+    setTargetSize(mapData.grid, width * cellD, height * cellD)
+    mapData.grid.innerHTML = new Array(width * height).fill('').map(()=> {
+      return `<div class="cell" style="width:${cellD}px; height:${cellD}px"></div>`
+    }).join('')
+  }
+
+  createGrid()
 
 
 }
